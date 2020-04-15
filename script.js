932877894cf1163f2a1b97fbcf56e3df65147fc7
Beibarys Rysbek <160107128@stu.sdu.edu.kz>
2020-04-15 18:30:18 +0600

    Front

---
+++ script.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+	function call() {
+	  var msg   = $('#formx').serialize();
+       $.ajax({
+         type: 'POST',
+         url: 'formx.php', //обращаемся к обработчику
+         data: msg,
+        success: function(data) {  //в случае успеха выводим результаты в div "results"
+            $('#formx').remove(); //скрываем форму после отправки 
+			$('#results').html(data); //показываем сообщение об успехе вместо неё 
+
+         },
+         error:  function(xhr, str){ //ошибка выводит соответствующее сообщение 
+	    alert('Возникла ошибка: ' + xhr.responseCode);
+         }
+       });
+   }
+</script>
